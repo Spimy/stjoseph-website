01e3d5b9f84a6f8749a05b17f0ba426b0feaf581
Spimy <officialspyme@gmail.com>
2020-08-22 22:06:17 +0400

    Add media urls

--- stjoseph/urls.py
+++ stjoseph/urls.py
@@ -16,11 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls import url
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('articles/', include('articles.urls'))
+    path('articles/', include('articles.urls')),
+    url(r'^media/(?P<path>.*)$',
+        serve,
+        {'document_root': settings.MEDIA_ROOT})
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
