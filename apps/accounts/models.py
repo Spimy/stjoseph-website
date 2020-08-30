f100a108d131b8f5a304a6a8cb3cfbd385b35f4f
Spimy <officialspyme@gmail.com>
2020-08-30 21:01:25 +0400

    Make username unique and case-insensitive

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 
 
 class Role(models.Model):
@@ -27,10 +27,22 @@ class Role(models.Model):
         return self.get_id_display()
 
 
+class EUserManager(UserManager):
+    def get_by_natural_key(self, username):
+        """ 
+        Override Django's default case-sensitive username check
+        This makes the username case-insensitve so only one user
+        can have a specific username regardless of its letter casing    
+        """
+        case_insensitive_username_field = f'{self.model.USERNAME_FIELD}__iexact'
+        return self.get(**{case_insensitive_username_field: username})
+
+
 class User(AbstractUser):
 
     avatar = models.ImageField(upload_to='users/avatars', blank=True)
     roles = models.ManyToManyField(Role, related_name='users', blank=True)
+    objects = EUserManager()
 
     def save(self, *args, **kwargs):
         super(User, self).save(*args, **kwargs)
