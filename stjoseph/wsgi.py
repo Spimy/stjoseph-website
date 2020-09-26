89f876f5c1030e1007bc93fb312a3a77da085054
Spimy <officialspyme@gmail.com>
2020-09-26 15:55:35 +0400

    Remove use of dotenv module

--- stjoseph/wsgi.py
+++ stjoseph/wsgi.py
@@ -11,9 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from dotenv import load_dotenv
-load_dotenv()
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stjoseph.settings.production')
 
 application = get_wsgi_application()
